92279560624b45e7c5aa47ec86d38bf30ea441e0
Jeff Swisher <jefftswisher@gmail.com>
2020-01-24 01:56:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! Show without stop/run. "
+    "Started my server using Shotgun!"
   end
 
 end
